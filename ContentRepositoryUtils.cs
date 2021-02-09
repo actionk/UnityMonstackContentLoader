424db26c1dc2657113507325759bef6502ac97b6
Andrey Cherkashin <action.open@gmail.com>
2021-02-09 14:35:55 +0100

    + tools

---
+++ ContentRepositoryUtils.cs
@@ -0,0 +1,20 @@
+﻿using Plugins.UnityMonstackCore.DependencyInjections;
+using UnityEngine;
+
+namespace Plugins.UnityMonstackContentLoader
+{
+    public static class ContentRepositoryUtils
+    {
+        public static T GetEntityByKey<T, TRepository, TKey>(TKey key)
+            where TRepository : AbstractContentListRepository<TKey, T>, new()
+            where T : class
+        {
+            var isApplicationPlaying = Application.isPlaying;
+            var repository = isApplicationPlaying ? DependencyProvider.Resolve<TRepository>() : new TRepository();
+            if (!isApplicationPlaying)
+                repository.Reload();
+
+            return repository.GetByKey(key);
+        }
+    }
+}
