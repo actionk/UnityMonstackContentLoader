2a800852d73d438466bcab02eea5958589dda2e6
Andrey Cherkashin <action.open@gmail.com>
2021-02-19 22:36:28 +0100

    + fixing single json

--- JSON/AbstractJSONContentSingleEntryRepository.cs
+++ JSON/AbstractJSONContentSingleEntryRepository.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using Plugins.UnityMonstackCore.DependencyInjections;
 using Plugins.UnityMonstackCore.Loggers;
 using Plugins.UnityMonstackCore.Utils;
+using UnityEngine;
 
 namespace Plugins.UnityMonstackContentLoader.JSON
 {
@@ -22,16 +23,32 @@ namespace Plugins.UnityMonstackContentLoader.JSON
         {
             try
             {
-                var dataAsByteArray = LocalStorageUtils.LoadBytesFromFile(FileSource, FilePath);
-                var reader = new StreamReader(new MemoryStream(dataAsByteArray));
-                var dataAsJson = reader.ReadToEnd();
-
+                var dataAsJson = ReadJson();
                 Entity = JsonConvert.DeserializeObject<T>(dataAsJson, JsonSerializerSettings);
+
+                if (Entity is IContentEntity contentEntity) contentEntity.Initialize();
             }
             catch (Exception e)
             {
                 UnityLogger.Error("Failed to load JSON file {} because {}", FilePath, e);
             }
+        }
+
+        private string ReadJson()
+        {
+            switch (FileSource)
+            {
+                case FileSourceType.Resources:
+                    return Resources.Load<TextAsset>(FilePath).text;
+
+                case FileSourceType.ApplicationPersistentData:
+                    var dataAsByteArray = LocalStorageUtils.LoadBytesFromFile(FileSource, FilePath);
+                    var reader = new StreamReader(new MemoryStream(dataAsByteArray));
+                    var dataAsJson = reader.ReadToEnd();
+                    return dataAsJson;
+            }
+
+            throw new NotImplementedException();
         }
     }
 }
