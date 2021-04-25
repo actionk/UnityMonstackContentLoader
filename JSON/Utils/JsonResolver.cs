13122c82cb448550a0327f2a11a08702b59bd350
Andrey Cherkashin <action.open@gmail.com>
2021-04-25 20:10:15 +0200

    + minor updates

---
+++ JSON/Utils/JsonResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Plugins.UnityMonstackCore.DependencyInjections;
+
+namespace Plugins.UnityMonstackContentLoader.JSON.Utils
+{
+    public abstract class JsonResolver<T> : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer) => throw new NotSupportedException("CustomCreationConverter should only be used while deserializing.");
+
+        public override bool CanConvert(Type objectType) => objectType == typeof(T);
+
+        public override bool CanWrite => false;
+
+        public T Create(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected abstract bool TryResolveByValue(JToken value, out T output);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var key = JToken.Load(reader);
+            if (TryResolveByValue(key, out T output))
+                return output;
+
+            throw new ApplicationException($"{GetType()} can't resolve object with key [{key}]");
+        }
+    }
+}
