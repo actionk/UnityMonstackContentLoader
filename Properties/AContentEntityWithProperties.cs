b5c62f18facf28b1acbd72dd92fc9d94508cae15
Andrey Cherkashin <action.open@gmail.com>
2021-05-03 19:22:05 +0200

    + sector generation

--- Properties/AContentEntityWithProperties.cs
+++ Properties/AContentEntityWithProperties.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Plugins.Shared.UnityMonstackCore.Utils;
 using Plugins.UnityMonstackContentLoader;
 using Plugins.UnityMonstackCore.Loggers;
 using UnityEngine;
@@ -16,13 +17,28 @@ namespace Plugins.Shared.UnityMonstackContentLoader.Properties
 
         public readonly List<ContentProperty> properties = new List<ContentProperty>();
 
-        private Dictionary<Type, ContentProperty> m_propertiesByType;
+        private MultiValueDictionary<Type, ContentProperty> m_propertiesByType;
 
         public T GetProperty<T>() where T : ContentProperty
         {
             try
             {
-                return (T) m_propertiesByType[typeof(T)];
+                return (T) m_propertiesByType[typeof(T)].FirstOrDefault();
+            }
+            catch (KeyNotFoundException)
+            {
+                UnityLogger.Error($"Content entity {this} has no property of type {typeof(T)}. Available properties: {string.Join(",\n", m_propertiesByType.Keys)}");
+                return null;
+            }
+        }
+
+        public List<T> GetPropertyList<T>() where T : ContentProperty
+        {
+            try
+            {
+                return m_propertiesByType[typeof(T)]
+                    .OfType<T>()
+                    .ToList();
             }
             catch (KeyNotFoundException)
             {
@@ -36,7 +52,7 @@ namespace Plugins.Shared.UnityMonstackContentLoader.Properties
             var key = typeof(T);
             if (m_propertiesByType.ContainsKey(key))
             {
-                value = (T) m_propertiesByType[key];
+                value = (T) m_propertiesByType[key].FirstOrDefault();
                 return true;
             }
 
@@ -52,7 +68,11 @@ namespace Plugins.Shared.UnityMonstackContentLoader.Properties
         private void InitializeProperties()
         {
             if (Application.isPlaying)
-                m_propertiesByType = properties.ToDictionary(x => x.GetType());
+            {
+                m_propertiesByType = new MultiValueDictionary<Type, ContentProperty>();
+                foreach (var property in properties)
+                    m_propertiesByType.Add(property.GetType(), property);
+            }
         }
     }
 }
