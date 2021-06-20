21da18c8b9537f9d095abd8c47e06bf9675c0e89
Andrey Cherkashin <action.open@gmail.com>
2021-06-20 14:45:05 +0200

    + more data in identifier

--- JSON/Converter/Identifiers/IdentifierAttribute.cs
+++ JSON/Converter/Identifiers/IdentifierAttribute.cs
@@ -5,6 +5,8 @@ namespace Plugins.Shared.UnityMonstackContentLoader.JSON.Converter.Identifiers
     public class IdentifierAttribute : Attribute
     {
         public string Id { get; }
+        public string Label { get; set; }
+        public string Tags { get; set; }
 
         public IdentifierAttribute(string id)
         {
